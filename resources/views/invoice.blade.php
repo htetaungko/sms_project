34c6938605362045886681e84062ced96d580336
yami web <yamiofweb@protonmail.com>
2019-01-20 19:38:36 +0630

    changed voucher design testing

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-8 offset-2">
                 <div class="invoice p-3 mb-3">
                 <!-- title row -->
                 <div class="row">
@@ -15,39 +15,7 @@
                     </div>
                     <!-- /.col -->
                 </div>
-                <!-- info row -->
-                <div class="row invoice-info">
-                    <div class="col-sm-4 invoice-col">
-                    From
-                    <address>
-                        <strong>Admin, Inc.</strong><br>
-                        795 Folsom Ave, Suite 600<br>
-                        San Francisco, CA 94107<br>
-                        Phone: (804) 123-5432<br>
-                        Email: info@almasaeedstudio.com
-                    </address>
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-sm-4 invoice-col">
-                    To
-                    <address>
-                        <strong>John Doe</strong><br>
-                        795 Folsom Ave, Suite 600<br>
-                        San Francisco, CA 94107<br>
-                        Phone: (555) 539-1037<br>
-                        Email: john.doe@example.com
-                    </address>
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-sm-4 invoice-col">
-                    <b>Invoice #007612</b><br>
-                    <br>
-                    <b>Order ID:</b> 4F3S8J<br>
-                    <b>Payment Due:</b> 2/22/2014<br>
-                    <b>Account:</b> 968-34567
-                    </div>
-                    <!-- /.col -->
-                </div>
+               
                 <!-- /.row -->
 
                 <!-- Table row -->
@@ -59,7 +27,6 @@
                         <th>Qty</th>
                         <th>Product</th>
                         <th>Serial #</th>
-                        <th>Description</th>
                         <th>Subtotal</th>
                         </tr>
                         </thead>
@@ -68,29 +35,14 @@
                         <td>1</td>
                         <td>Call of Duty</td>
                         <td>455-981-221</td>
-                        <td>El snort testosterone trophy driving gloves handsome</td>
+                        
                         <td>$64.50</td>
                         </tr>
                         <tr>
                         <td>1</td>
                         <td>Need for Speed IV</td>
                         <td>247-925-726</td>
-                        <td>Wes Anderson umami biodiesel</td>
                         <td>$50.00</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                        <td>Monsters DVD</td>
-                        <td>735-845-642</td>
-                        <td>Terry Richardson helvetica tousled street art master</td>
-                        <td>$10.70</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                        <td>Grown Ups Blue Ray</td>
-                        <td>422-568-642</td>
-                        <td>Tousled lomo letterpress</td>
-                        <td>$25.99</td>
                         </tr>
                         </tbody>
                     </table>
@@ -100,20 +52,7 @@
                 <!-- /.row -->
 
                 <div class="row">
-                    <!-- accepted payments column -->
-                    <div class="col-6">
-                    <p class="lead">Payment Methods:</p>
-                    <img src="../../dist/img/credit/visa.png" alt="Visa">
-                    <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-                    <img src="../../dist/img/credit/american-express.png" alt="American Express">
-                    <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
-
-                    <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
-                        Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                        plugg
-                        dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
-                    </p>
-                    </div>
+                    <div class="col-6"></div>
                     <!-- /.col -->
                     <div class="col-6">
                     <p class="lead">Amount Due 2/22/2014</p>
@@ -129,10 +68,6 @@
                             <td>$10.34</td>
                         </tr>
                         <tr>
-                            <th>Shipping:</th>
-                            <td>$5.80</td>
-                        </tr>
-                        <tr>
                             <th>Total:</th>
                             <td>$265.24</td>
                         </tr>
@@ -146,7 +81,7 @@
                 <!-- this row will not appear when printing -->
                 <div class="row no-print">
                     <div class="col-12">
-                    <a href="" @click.prevent="printme" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
+                    <a href="" @click.prevent="printme" target="_blank" class="btn btn-danger"><i class="fa fa-print"></i> Print</a>
                     <button type="button" class="btn btn-success float-right"><i class="fa fa-credit-card"></i> Submit
                         Payment
                     </button>
